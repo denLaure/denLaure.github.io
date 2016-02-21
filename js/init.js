78773429796b1a0865f536b65b48a2ba54322016
Denis Laure <den.a.laure@gmail.com>
2016-02-21 21:05:35 +0100

    Add hash navigation

--- js/init.js
+++ js/init.js
@@ -1,27 +1,32 @@
 $( document ).ready(function() {
-    moveFromTop("#aboutMeCard");
     $('.modal-trigger').leanModal();
     $("#publicationsLink").click(function() {
         $("#publicationsTab").trigger("click");
     });
     $("#aboutMeTab").click(function() {
         moveFromTop("#aboutMeCard");
+        window.location.href = window.location.href.replace(/#(.*)$/, '') + '#about';
     });
     $("#educationTab").click(function() {
         moveFromTop("#educationCard");
+        window.location.href = window.location.href.replace(/#(.*)$/, '') + '#education';
     });
     $("#skillsTab").click(function() {
         moveFromTop("#skillsCard");
+        window.location.href = window.location.href.replace(/#(.*)$/, '') + '#skills';
     });
     $("#projectsTab").click(function() {
         moveFromTop("#projectsCard");
+        window.location.href = window.location.href.replace(/#(.*)$/, '') + '#projects';
     });
     $("#awardsTab").click(function() {
         moveFromTop("#awardsCard");
+        window.location.href = window.location.href.replace(/#(.*)$/, '') + '#awards';
     });
     $("#publicationsTab").click(function() {
         $("#publicationsList").load("publications.html");
         moveFromTop("#publicationsCard");
+        window.location.href = window.location.href.replace(/#(.*)$/, '') + '#publications';
     });
 });
 
